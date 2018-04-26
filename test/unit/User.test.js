be6a1fb7e680cee8f6cd119fe6cf5da45d6fe1ec
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-26 10:42:24 -0700

    generate hash from password test passing.

--- test/unit/User.test.js
+++ test/unit/User.test.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 const User = require('../../lib/models/User');
 
-describe('User model', () => {
+describe.only('User model', () => {
 
     const data = {
         email: 'me@me.com'
@@ -9,10 +9,10 @@ describe('User model', () => {
 
     const password = 'abc';
 
-    it('generates hash from password', () =>{
+    it('generates hash from password', () =>{ //is this an actual token generated.
         const user = new User(data);
         user.generateHash(password); //generates a hash using the password.
-        assert.ok(user.hash);
+        assert.ok(user.hash); //hash is a generated string of random characters. hashing is the act of converting those passwords to unreadable strings.
         assert.notEqual(user.hash, password); //this is saying that the hash should not be the same as the password. 
 
 
